646214af5a6a3555bab7776f904d53141c4b9875
Peter van der Woude <pandawoude@gmail.com>
2008-01-15 03:50:52 -0800

    tidy up/further Workspace tests (so far just DockManager and XtraTab) - tests are very light so far

--- CABDevExpress.ExtensionKit.Tests/WorkspaceTests.cs
+++ CABDevExpress.ExtensionKit.Tests/WorkspaceTests.cs
@@ -9,35 +9,41 @@ namespace CABDevExpress.ExtensionKit.Tests
 {
 	public class WorkspaceTests
 	{
-		readonly TestableWorkItem _workItem = new TestableWorkItem();
 		readonly TestableSmartPart _smartPart = new TestableSmartPart();
 
-		const string TestableSmartPart = "TestableSmartPart";
-
 		[Fact]
-		public void CanSomethingDockManagerWorkspace()
+		public void CanShowAndCloseDockManagerWorkspace()
 		{
-			//TODO this condition below, won't work so create an exception for the user to know
-//			DockManagerWorkspace dockManager = new DockManagerWorkspace(new DockManager());
 			DockManagerWorkspace dockManager = new DockManagerWorkspace(new DockManager(new ContainerControl()));
 			dockManager.Show(_smartPart);
 
 			Assert.Equal(1, dockManager.DockPanels.Count);
+
+			dockManager.Close(_smartPart);
+
+			Assert.Equal(0, dockManager.DockPanels.Count);
 		}
 
 		[Fact]
-		public void CanCreateXtraTabWorkspace()
+		public void CanShowAndCloseXtraTabWorkspace()
 		{
-			XtraTabWorkspace xtrab = new XtraTabWorkspace();
+			Font tahoma9pt = new Font("Tahoma", 9.75f);
+
 			XtraTabSmartPartInfo info = new XtraTabSmartPartInfo();
-
 			info.Text = "text";
-			info.PageHeaderFont = new Font("Tahoma", 9.75f);
+			info.PageHeaderFont = tahoma9pt;
 
+			XtraTabWorkspace xtrab = new XtraTabWorkspace();
 			xtrab.Show(_smartPart, info);
 
 			Assert.Equal(1, xtrab.TabPages.Count);
 			Assert.Equal(1, xtrab.SmartParts.Count);
+			Assert.Equal("text", xtrab.SelectedTabPage.Text);
+			Assert.Equal(tahoma9pt, xtrab.SelectedTabPage.Appearance.Header.Font);
+
+			xtrab.Close(_smartPart);
+			Assert.Equal(0, xtrab.TabPages.Count);
+			Assert.Equal(0, xtrab.SmartParts.Count);
 		}
 	}
 }
