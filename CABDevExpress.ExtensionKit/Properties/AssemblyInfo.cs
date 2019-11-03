fbecc404a37064f60313e6b6925163d07649505d
fabio.borghi <fabio.borghi@J9B40ET>
2019-11-03 18:52:23 +0100

    Upgrade to DevExpress 19.2.3

--- CABDevExpress.ExtensionKit/Properties/AssemblyInfo.cs
+++ CABDevExpress.ExtensionKit/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("19.1.7.0")]
-[assembly: AssemblyFileVersion("19.1.7.0")]
+[assembly: AssemblyVersion("19.2.3.0")]
+[assembly: AssemblyFileVersion("19.2.3.0")]
