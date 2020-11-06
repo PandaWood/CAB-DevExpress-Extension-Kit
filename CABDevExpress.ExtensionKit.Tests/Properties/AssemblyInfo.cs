ee0d9e1b529379d5294445a67ef67b3bae40a48b
fabio.borghi <fabio.borghi@J9B40ET>
2020-11-06 09:57:00 +0100

    Upgrade to DevExpress 20.1.8

--- CABDevExpress.ExtensionKit.Tests/Properties/AssemblyInfo.cs
+++ CABDevExpress.ExtensionKit.Tests/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("20.1.6.0")]
-[assembly: AssemblyFileVersion("20.1.6.0")]
+[assembly: AssemblyVersion("20.1.8.0")]
+[assembly: AssemblyFileVersion("20.1.8.0")]
