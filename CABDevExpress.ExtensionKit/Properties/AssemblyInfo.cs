baff933c3a6790f07c4cce49eaa3e051f7e9d36e
fabio.borghi <fabio.borghi@J9B40ET>
2021-10-02 15:16:15 +0200

    Upgrade to DevExpress 21.1.6

--- CABDevExpress.ExtensionKit/Properties/AssemblyInfo.cs
+++ CABDevExpress.ExtensionKit/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("20.2.5.0")]
-[assembly: AssemblyFileVersion("20.2.5.0")]
+[assembly: AssemblyVersion("21.1.6.0")]
+[assembly: AssemblyFileVersion("21.1.6.0")]
