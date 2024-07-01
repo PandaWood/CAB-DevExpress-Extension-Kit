54066b7a8009129b3ff8ef6fe4a9cdb9e16f60fb
fabio.borghi <fabio.borghi@J9B40ET>
2024-07-01 08:22:04 +0200

    Upgrade to DevExpress 23.2.7

--- CABDevExpress.ExtensionKit.Tests/Properties/AssemblyInfo.cs
+++ CABDevExpress.ExtensionKit.Tests/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("23.2.6.0")]
-[assembly: AssemblyFileVersion("23.2.6.0")]
+[assembly: AssemblyVersion("23.2.7.0")]
+[assembly: AssemblyFileVersion("23.2.7.0")]
