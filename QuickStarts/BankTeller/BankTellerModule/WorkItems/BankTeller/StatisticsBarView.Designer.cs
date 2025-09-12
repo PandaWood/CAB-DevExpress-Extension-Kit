edaf117256bc35742283af1b7b82087cc16fbcf2
fabio.borghi <fabio.borghi@J9B40ET>
2025-09-12 14:54:44 +0200

    Upgrade to DevExpress vr. 25.1.5

--- QuickStarts/BankTeller/BankTellerModule/WorkItems/BankTeller/StatisticsBarView.Designer.cs
+++ QuickStarts/BankTeller/BankTellerModule/WorkItems/BankTeller/StatisticsBarView.Designer.cs
@@ -28,63 +28,64 @@ namespace BankTellerModule.WorkItems.BankTeller
         /// </summary>
         private void InitializeComponent()
         {
-			DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
-			DevExpress.XtraCharts.SecondaryAxisX secondaryAxisX1 = new DevExpress.XtraCharts.SecondaryAxisX();
-			DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
-			DevExpress.XtraCharts.Series series2 = new DevExpress.XtraCharts.Series();
-			DevExpress.XtraCharts.ChartTitle chartTitle1 = new DevExpress.XtraCharts.ChartTitle();
-			this.infoProvider = new Microsoft.Practices.CompositeUI.SmartParts.SmartPartInfoProvider();
-			this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
-			((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(secondaryAxisX1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(series2)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// chartControl1
-			// 
-			xyDiagram1.AxisY.Range.SideMarginsEnabled = true;
-			xyDiagram1.AxisX.Range.SideMarginsEnabled = true;
-			secondaryAxisX1.Name = "secondaryAxisX1";
-			secondaryAxisX1.Range.SideMarginsEnabled = true;
-			xyDiagram1.SecondaryAxesX.AddRange(new DevExpress.XtraCharts.SecondaryAxisX[] {
-            secondaryAxisX1});
-			this.chartControl1.Diagram = xyDiagram1;
-			this.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.chartControl1.Location = new System.Drawing.Point(0, 0);
-			this.chartControl1.Name = "chartControl1";
-			series1.PointOptionsTypeName = "PointOptions";
-			series1.Name = "Series 1";
-			series2.PointOptionsTypeName = "PointOptions";
-			series2.Name = "Series 2";
-			this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
-        series1,
-        series2};
-			this.chartControl1.SeriesTemplate.PointOptionsTypeName = "PointOptions";
-			this.chartControl1.Size = new System.Drawing.Size(167, 230);
-			this.chartControl1.TabIndex = 0;
-			chartTitle1.Text = "Stats";
-			this.chartControl1.Titles.AddRange(new DevExpress.XtraCharts.ChartTitle[] {
-            chartTitle1});
-			// 
-			// StatsBarView
-			// 
-			this.Controls.Add(this.chartControl1);
-			this.Name = "StatsBarView";
-			this.Size = new System.Drawing.Size(167, 230);
-			((System.ComponentModel.ISupportInitialize)(secondaryAxisX1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(series2)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
-			this.ResumeLayout(false);
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.SecondaryAxisX secondaryAxisx1 = new DevExpress.XtraCharts.SecondaryAxisX();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.Series series2 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.ChartTitle chartTitle1 = new DevExpress.XtraCharts.ChartTitle();
+            infoProvider = new Microsoft.Practices.CompositeUI.SmartParts.SmartPartInfoProvider();
+            chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            ((System.ComponentModel.ISupportInitialize)chartControl1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)xyDiagram1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)secondaryAxisx1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)series1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)series2).BeginInit();
+            SuspendLayout();
+            // 
+            // chartControl1
+            // 
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            secondaryAxisx1.AxisID = 0;
+            secondaryAxisx1.Name = "secondaryAxisX1";
+            secondaryAxisx1.VisibleInPanesSerializable = "-1";
+            xyDiagram1.SecondaryAxesX.AddRange(new DevExpress.XtraCharts.SecondaryAxisX[] { secondaryAxisx1 });
+            chartControl1.Diagram = xyDiagram1;
+            chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            chartControl1.Location = new System.Drawing.Point(0, 0);
+            chartControl1.Name = "chartControl1";
+            series1.Name = "Series 1";
+            series1.SeriesID = 0;
+            series2.Name = "Series 2";
+            series2.SeriesID = 1;
+            chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[]
+    {
+    series1,
+    series2
+    };
+            chartControl1.Size = new System.Drawing.Size(745, 230);
+            chartControl1.TabIndex = 0;
+            chartTitle1.Text = "Stats";
+            chartTitle1.TitleID = 0;
+            chartControl1.Titles.AddRange(new DevExpress.XtraCharts.ChartTitle[] { chartTitle1 });
+            // 
+            // StatisticsBarView
+            // 
+            Controls.Add(chartControl1);
+            Name = "StatisticsBarView";
+            Size = new System.Drawing.Size(745, 230);
+            ((System.ComponentModel.ISupportInitialize)secondaryAxisx1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)xyDiagram1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)series1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)series2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chartControl1).EndInit();
+            ResumeLayout(false);
 
         }
 
         #endregion
 
-		private Microsoft.Practices.CompositeUI.SmartParts.SmartPartInfoProvider infoProvider;
+        private Microsoft.Practices.CompositeUI.SmartParts.SmartPartInfoProvider infoProvider;
 		private DevExpress.XtraCharts.ChartControl chartControl1;
     }
 }
